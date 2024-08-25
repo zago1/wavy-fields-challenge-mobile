35afe19cf1d8ec697cebeb3524ff26d939426f93
Igor Massaro <igorzagomassaro@gmail.com>
2024-08-25 15:38:11 -0300

    first commit

---
+++ src/screens/Signup.tsx
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+type Props = {}
+
+const Signup = (props: Props) => {
+  return (
+    <View>
+      <Text>Signup</Text>
+    </View>
+  )
+}
+
+export default Signup
+
+const styles = StyleSheet.create({})
---
+++ src/screens/Todos.tsx
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+type Props = {}
+
+const Todos = (props: Props) => {
+  return (
+    <View>
+      <Text>Todos</Text>
+    </View>
+  )
+}
+
+export default Todos
+
+const styles = StyleSheet.create({})
