06e9a8a40c94e7dfde1f6030174a01164df90338
Igor Massaro <igorzagomassaro@gmail.com>
2024-08-25 20:28:39 -0300

    remove useless code and add StatusBar

--- App.tsx
+++ App.tsx
@@ -1,21 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
 import Router from './src/Router';
 import { AuthProvider } from './src/data/contexts/AuthContext';
 
 export default function App() {
   return (
     <AuthProvider>
+      <StatusBar style="inverted" />
       <Router />
     </AuthProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
