81581ee8c2ea0d58558afef0833a5fc5631d76cf
Igor Massaro <igorzagomassaro@gmail.com>
2024-08-26 16:12:37 -0300

    add createTodo implementation; add pagination implementation

--- src/data/contexts/TodosContext.tsx
+++ src/data/contexts/TodosContext.tsx
@@ -8,42 +8,99 @@ interface ITodosContext {
   doneTodos: Todo[];
   todoToEdit?: Todo;
   loading: boolean;
+  listLoading: boolean;
+  error: boolean;
+  haveMore: boolean;
   addTodoToEdit(id: string): void;
   toggleDone(id: string, done: boolean): Promise<void>;
-  createTodo(text: string): Promise<void>;
+  createTodo(text: string): Promise<boolean>;
   editTodo(id: string, text: string): Promise<void>;
   deleteTodo(id: string): Promise<void>;
+  loadMore(): Promise<void>;
+  resetTodos(): void;
+  retry(): void;
 }
+
+const PAGE_SIZE = 20;
 
 export const TodosContext = createContext({} as ITodosContext);
 
 export const TodosProvider = ({ children }: PropsWithChildren) => {
   const [todos, setTodos] = useState<Todo[]>([]);
   const [todoToEdit, setTodoToEdit] = useState<Todo>();
+
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(0);
+  const [listLoading, setListLoading] = useState(false);
+  const [error, setError] = useState(false);
+
   const [loading, setLoading] = useState<boolean>(false);
 
-  const { httpGet, httpPatch, httpDelete } = useAPI();
+  const { httpGet, httpPatch, httpDelete, httpPost } = useAPI();
   const { userId } = useContext(AuthContext);
 
-  const loadTodos = useCallback(async () => {
+  const loadTodos = useCallback(async (page: number = 1) => {
+    if (listLoading) { return; }
     if (!userId) { return };
 
-    setLoading(true);
-    const response = await httpGet(`/task/list/${userId}`);
+    setError(false);
+    setListLoading(true);
+    const { data, ok, error } = await httpGet(`/task/list/${userId}?page=${page}&pageSize=${PAGE_SIZE}`);
 
-    if (!response.ok) {
-      alert('Error while fetching todos! 123');
-      setLoading(false);
+    if (!ok) {
+      console.log('[error]', error);
+      alert('Error while fetching todos!');
+      setListLoading(false);
+      setError(true);
       return;
     }
 
-    setTodos(response.data.data);
-    setLoading(false);
-  }, [userId]);
+    if (totalPages === 0) {
+      setTotalPages(Math.ceil(data.metadata.totalRegisters / PAGE_SIZE));
+    }
+
+    setTodos(currentTodos => [...currentTodos, ...data.data]);
+    setListLoading(false);
+  }, [userId, totalPages, listLoading]);
+
+  const loadMore = useCallback(async () => {
+    const nextPage = currentPage + 1;
+    if (nextPage > totalPages) { 
+      alert('No more Todos to load!');
+      return; 
+    }
+
+    await loadTodos(nextPage);
+    setCurrentPage(nextPage);
+  }, [currentPage, loadTodos, totalPages]);
 
   const createTodo = useCallback(async (text: string) => {
+    if (!text) { 
+      alert('Input empty!');
+      return false;
+    }
 
-  }, [userId]);
+    setLoading(true);
+    const response = await httpPost('/task', { text, userId });
+
+    if (!response.ok) {
+      alert('Error while creating Todo!');
+      setLoading(false);
+      return false;
+    }
+
+    if (totalPages > currentPage) {
+      setLoading(false);
+      return true;
+    }
+
+    const newTodos = [...todos];
+    newTodos.push(response.data);
+
+    setTodos(newTodos);
+    setLoading(false);
+    return true;
+  }, [userId, todos, currentPage, totalPages]);
 
   const editTodo = useCallback(async (id: string, text: string) => {
     setLoading(true);
@@ -103,10 +160,24 @@ export const TodosProvider = ({ children }: PropsWithChildren) => {
     setTodoToEdit(todo);
   }, [todos]);
 
+  const resetTodos = () => {
+    setTodos([]);
+    setError(false);
+    setCurrentPage(1);
+    setTotalPages(0);
+    setListLoading(false);
+    setLoading(false);
+    setTodoToEdit(undefined);
+  }
+
 
   const doneTodos = useMemo(() => {
     return todos.filter(todo => todo.done);
   }, [todos]);
+
+  const haveMore = useMemo(() => {
+    return totalPages > currentPage;
+  }, [totalPages, currentPage]);
 
   useEffect(() => {
     loadTodos();
@@ -118,11 +189,17 @@ export const TodosProvider = ({ children }: PropsWithChildren) => {
       doneTodos,
       todoToEdit,
       loading,
+      listLoading,
+      error,
+      haveMore,
       addTodoToEdit,
       toggleDone,
       createTodo,
       editTodo,
       deleteTodo,
+      loadMore,
+      resetTodos,
+      retry: loadTodos
     }}>
       { children }
     </TodosContext.Provider>
