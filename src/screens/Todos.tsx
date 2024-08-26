1745394fa46a764af03ef1c4f3afc63a724f9cec
Igor Massaro <igorzagomassaro@gmail.com>
2024-08-25 23:18:30 -0300

    add TodoList component to Todos screen

--- src/screens/Todos.tsx
+++ src/screens/Todos.tsx
@@ -1,16 +1,27 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useContext, useEffect } from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import TodoList from '../components/TodoList'
+import { TodosContext } from '../data/contexts/TodosContext'
 
 type Props = {}
 
 const Todos = (props: Props) => {
+
   return (
-    <View>
-      <Text>Todos</Text>
-    </View>
+    <SafeAreaView style={styles.screen}>
+      <TodoList />
+    </SafeAreaView>
   )
 }
 
-export default Todos
+export default Todos;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  screen: {
+    display: 'flex',
+    flex: 1,
+    padding: 32,
+    backgroundColor: '#1a1c20',
+  }
+})
